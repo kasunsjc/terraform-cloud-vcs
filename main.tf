4db31a24713d82de1a6f1c8e014f2c1262b41680
Kasun Rajapakse <kasunsjc@hotmail.com>
2022-02-07 11:43:13 +0000

    fix typo

--- main.tf
+++ main.tf
@@ -9,5 +9,5 @@ resource "azurerm_virtual_network" "virtual-network" {
   name                = var.vnet_name
   resource_group_name = azurerm_resource_group.resource-group.name
   location            = azurerm_resource_group.resource-group.location
-  address_space       = [var.vnet_cidr]
+  address_space       = var.vnet_cidr
 }
