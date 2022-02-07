d82092417af2780edd82e67501bcc1f82e8655f3
Kasun Rajapakse <kasunsjc@hotmail.com>
2022-02-07 12:32:16 +0000

    Add default subnet

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ resource "azurerm_virtual_network" "virtual-network" {
   location            = azurerm_resource_group.resource-group.location
   address_space       = var.vnet_cidr
 }
+
+resource "azurerm_subnet" "vm-subnet" {
+  name = "default"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  location = azurerm_resource_group.resource-group.location
+  virtual_network_name = azurerm_virtual_network.virtual-network.name
+  address_prefix = "10.10.1.0/24"
+}
